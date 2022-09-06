cb9d9e0f347340c622770c771fd687fa453ddf65
Enzo <farinot.enzo@gmail.com>
2022-09-06 17:58:13 +0200

    correction petite erreur 0.0 to -> 0 avec Math.floor()

--- front/src/app/accueil-collab/gestion-planning/resume-mois/resume-mois.component.ts
+++ front/src/app/accueil-collab/gestion-planning/resume-mois/resume-mois.component.ts
@@ -4,6 +4,8 @@ import {EventsService} from "../../../service/events.service";
 import {FormBuilder} from "@angular/forms";
 import {Router} from "@angular/router";
 import {LoginService} from "../../../service/login.service";
+import * as moment from "moment/moment";
+import {months} from "moment/moment";
 
 @Component({
   selector: 'app-resume-mois',
@@ -22,6 +24,16 @@ export class ResumeMoisComponent implements OnInit {
   // Le timestamp d'ancrage dans le mois traité, il est en seconde donc /1000
   tms:number;
 
+  // Une valeur qui contient la date actuelle pour se servir de 'moment'
+  date:Date;
+
+  // Ici les timestamp qui sont les limites du range mensuel
+  //tmsBegin:number;
+  //tmsEnd:number;
+
+  // Valeur string à afficher
+  actualMonth:String;
+
   constructor(
     private fb:FormBuilder,
     private _eventsService:EventsService,
@@ -30,8 +42,16 @@ export class ResumeMoisComponent implements OnInit {
   ) {
     this.eventsService=_eventsService;
     this.loginService = _loginService;
+
     this.mois = 0;
+    this.date = new Date(moment().format());
+
+    // on récupère le tms
     this.tms = Math.floor(Date.now()/1000);
+
+    // Le mois qui sera affiché
+    this.actualMonth = moment().format('MMMM');
+
     this.refreshEvents();
   }
 
@@ -54,22 +74,35 @@ export class ResumeMoisComponent implements OnInit {
         this.eventsList = response;
       }
     )
+    this.ngOnInit();
   }
 
   nextMonth(){
     this.mois+=1;
     this.tms+=2764800; // 32 jour en secondes 604800 + 200 sec par sécurité
+    this.date.setMonth(this.date.getMonth()+1);
+    this.actualMonth = moment(this.date).format('MMMM');
     this.refreshEvents();
   }
 
   previousMonth(){
     this.mois-=1;
     this.tms-=2764800;
+    this.date.setMonth(this.date.getMonth()-1);
+    this.actualMonth = moment(this.date).format('MMMM');
     this.refreshEvents();
   }
 
   editer(event:Events){
     this.router.navigate(['event/'+event.id]);
+  }
+
+  reset(){
+    this.mois=0;
+    this.tms=Math.floor(Date.now()/1000);
+    this.date= new Date(moment().format());
+    this.actualMonth = moment(this.date).format('MMMM');
+    this.refreshEvents();
   }
 
 }
